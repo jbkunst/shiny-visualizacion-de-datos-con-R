7dcdcdb1336eb5ff5b87a080c5fcc81b13fef4cc
Joshua Kunst <jbkunst@gmail.com>
2022-08-04 16:18:56 -0400

    updates

--- R/99-slides-xarginan-2-pdf.R
+++ R/99-slides-xarginan-2-pdf.R
@@ -1,7 +1,13 @@
 source("https://git.io/xaringan2pdf")
 
 
-# xaringan_to_pdf("clase-04.html")
+rmarkdown::render("clase-01.Rmd")
+rmarkdown::render("clase-02.Rmd")
+rmarkdown::render("clase-03.Rmd")
+# rmarkdown::render("clase-04.Rmd")
+
+
 xaringan_to_pdf("clase-03.html")
 xaringan_to_pdf("clase-02.html")
 xaringan_to_pdf("clase-01.html")
+# xaringan_to_pdf("clase-04.html")
