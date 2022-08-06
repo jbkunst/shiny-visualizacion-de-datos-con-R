b73c4388cfe8a351db68590435254f36104db318
Joshua Kunst <jbkunst@gmail.com>
2022-08-06 11:02:08 -0400

    actualizando clase 4

--- R/99-slides-xarginan-2-pdf.R
+++ R/99-slides-xarginan-2-pdf.R
@@ -1,11 +1,11 @@
 source("https://git.io/xaringan2pdf")
 
-rmarkdown::render("clase-01.Rmd")
-rmarkdown::render("clase-02.Rmd")
-rmarkdown::render("clase-03.Rmd")
+# rmarkdown::render("clase-01.Rmd")
+# rmarkdown::render("clase-02.Rmd")
+# rmarkdown::render("clase-03.Rmd")
 rmarkdown::render("clase-04.Rmd")
 
-xaringan_to_pdf("clase-01.html")
-xaringan_to_pdf("clase-02.html")
-xaringan_to_pdf("clase-03.html")
+# xaringan_to_pdf("clase-01.html")
+# xaringan_to_pdf("clase-02.html")
+# xaringan_to_pdf("clase-03.html")
 xaringan_to_pdf("clase-04.html")
---
+++ apps/app-sismos-mapa-tabla.R
@@ -0,0 +1,81 @@
+# Ejercio inicial de la clase 3
+library(shiny);library(leaflet);library(rvest);library(dplyr);library(janitor)
+library(tidyverse);library(lubridate);library(DT);library(bslib)
+
+descarga_sismos_por_dia <- function(fecha_string){
+  message(fecha_string)
+  fecha <- ymd(fecha_string)
+  y <- year(fecha)
+  m <- format(fecha, "%m")  
+  d <- format(fecha, "%Y%m%d")  
+  url <- str_glue("https://www.sismologia.cl/sismicidad/catalogo/{y}/{m}/{d}.html")
+  datos <- read_html(url) |>
+    html_table() |>
+    dplyr::nth(2) |>
+    janitor::clean_names() |>
+    tidyr::separate(latitud_longitud, into = c("latitud", "longitud"),
+                    sep = " ", convert = TRUE
+    )
+  datos
+}
+
+ui <- navbarPage(
+  theme = bs_theme(
+    "navbar-light-bg"  = "#002884",
+    primary = "#5583ff",
+    base_font = font_google("Nunito")
+  ),
+  # "VerSismos", 
+  tags$span(
+    "Sismos en ",
+    tags$img(src = "https://portales.bancochile.cl/uploads/000/035/565/2ca8e2c5-606c-47f4-80ef-03bec528775d/original/bch-inverse.svg"),  
+  ),
+  tabPanel(
+    "Sismos",
+    sidebarLayout(
+      sidebarPanel(
+        width = 2,
+        dateInput(
+          "fecha",
+          label = h5("Seleccione fecha por favor"),
+          max = Sys.Date()
+        ),
+        actionButton("primary", "Primary", icon("r-project"), class = "btn-primary")
+      ),
+      mainPanel(
+        width = 10,
+        fluidRow(
+          column(width = 4, leafletOutput("mapa")),
+          column(width = 8, DTOutput("tabla"))
+        )
+      )
+    )
+  )
+)
+
+server <- function(input, output){
+  
+  output$mapa <- renderLeaflet({
+    
+    datos <- descarga_sismos_por_dia(input$fecha)
+    
+    leaflet(datos) |>
+      addTiles() |>
+      addMarkers(
+        lng = ~longitud,
+        lat = ~latitud,
+        popup = ~as.character(magnitud_2),
+        label = ~as.character(`fecha_local_lugar`)
+      )
+  })
+  
+  output$tabla <- renderDataTable({
+    
+    datos <- descarga_sismos_por_dia(input$fecha)
+    
+    datatable(datos)
+  })
+  
+}
+
+shinyApp(ui, server)
