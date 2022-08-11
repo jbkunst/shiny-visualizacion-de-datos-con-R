cfb00c50bd5d71d90b0ad0bce8f16cacf6d7517e
Joshua Kunst <jbkunst@gmail.com>
2022-08-10 20:29:07 -0400

    actualizando clase 3

--- apps/temas/01-app/app.R
+++ apps/temas/01-app/app.R
@@ -6,7 +6,8 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins","Number of bins:", min = 1, max = 50, value = 30),
-      textInput("titulo", "Ingrese titulo")
+      textInput("titulo", "Ingrese titulo"),
+      actionButton("boton", "No hago nada", class = "btn-primary")
     ),
     mainPanel(plotOutput("distPlot"))
   )
--- apps/temas/02-app-bslib-bootswatch/app.R
+++ apps/temas/02-app-bslib-bootswatch/app.R
@@ -8,7 +8,8 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("bins","Number of bins:", min = 1, max = 50, value = 30),
-      textInput("titulo", "Ingrese titulo")
+      textInput("titulo", "Ingrese titulo"),
+      actionButton("boton", "No hago nada", class = "btn-primary")
     ),
     mainPanel(plotOutput("distPlot"))
   )
